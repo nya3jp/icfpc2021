16ae48b4a28f0fc117bcf158f9160a9b938fe81e
chiro <i@chir.jp>
2021-07-09 22:52:53 +0900

    ui: Add types.ts and problem_fetcher.ts.

--- third_party/ui/src/index.ts
+++ third_party/ui/src/index.ts
@@ -1,21 +1,7 @@
-type Point = [number, number];
+import {Figure, Hole, Problem} from './types';
+import { fetch_problem } from './problem_fetcher';
 
-type Edge = [number, number];
-
-type Hole = Point[];
-
-interface Figure {
-    edges: Edge[]
-    vertices: Point[]
-}
-
-interface Problem {
-    hole: Hole
-    figure: Figure
-    epsilon: number
-}
-
-const theProblem: Problem = {"hole":[[45,80],[35,95],[5,95],[35,50],[5,5],[35,5],[95,95],[65,95],[55,80]],"epsilon":150000,"figure":{"edges":[[2,5],[5,4],[4,1],[1,0],[0,8],[8,3],[3,7],[7,11],[11,13],[13,12],[12,18],[18,19],[19,14],[14,15],[15,17],[17,16],[16,10],[10,6],[6,2],[8,12],[7,9],[9,3],[8,9],[9,12],[13,9],[9,11],[4,8],[12,14],[5,10],[10,15]],"vertices":[[20,30],[20,40],[30,95],[40,15],[40,35],[40,65],[40,95],[45,5],[45,25],[50,15],[50,70],[55,5],[55,25],[60,15],[60,35],[60,65],[60,95],[70,95],[80,30],[80,40]]}};
+const theProblem: Problem = fetch_problem(0);
 
 const theCanvas = document.getElementById('canvas') as HTMLCanvasElement;
 
