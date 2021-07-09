da5d276a9596abec6d388f3d10671c0d0f874b4e
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2021-07-09 23:22:43 +0900

    ui: Ugoku

--- third_party/ui/src/index.ts
+++ third_party/ui/src/index.ts
@@ -7,6 +7,10 @@ function distance(p: Point, q: Point) {
     return dx * dx + dy * dy;
 }
 
+function roundPoint(p: Point): Point {
+    return [Math.round(p[0]), Math.round(p[1])];
+}
+
 class Translator {
     constructor(public zoom: number = 5.0) {}
 
@@ -39,27 +43,29 @@ class UI {
 
     private draw() {
         const ctx = this.canvas.getContext('2d')!;
-        this.drawHole(ctx, this.problem.hole);
-        this.drawFigure(ctx, this.problem.figure);
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawHole(ctx);
+        this.drawFigure(ctx);
     }
 
-    private drawHole(ctx: CanvasRenderingContext2D, hole: Hole) {
+    private drawHole(ctx: CanvasRenderingContext2D) {
         ctx.strokeStyle = 'rgb(0, 0, 0)';
         ctx.beginPath();
-        ctx.moveTo(...this.translator.modelToCanvas(hole[hole.length - 1]));
+        ctx.moveTo(...this.translator.modelToCanvas(this.problem.hole[this.problem.hole.length - 1]));
         for (const v of this.problem.hole) {
             ctx.lineTo(...this.translator.modelToCanvas(v));
         }
         ctx.stroke();
     }
 
-    private drawFigure(ctx: CanvasRenderingContext2D, figure: Figure) {
-        const {edges, vertices} = figure;
+    private drawFigure(ctx: CanvasRenderingContext2D) {
+        const edges = this.problem.figure.edges;
+        const pose = this.pose;
         ctx.strokeStyle = 'rgb(255, 0, 0)';
         for (const edge of edges) {
             ctx.beginPath();
-            ctx.moveTo(...this.translator.modelToCanvas(vertices[edge[0]]));
-            ctx.lineTo(...this.translator.modelToCanvas(vertices[edge[1]]));
+            ctx.moveTo(...this.translator.modelToCanvas(pose[edge[0]]));
+            ctx.lineTo(...this.translator.modelToCanvas(pose[edge[1]]));
             ctx.stroke();
         }
     }
@@ -68,16 +74,16 @@ class UI {
         if (ev.button !== 0) {
             return;
         }
-        const click = this.translator.canvasToModel([ev.offsetX, ev.offsetY]);
+        const pos = this.translator.canvasToModel([ev.offsetX, ev.offsetY]);
         let nearest = 0;
         for (let i = 0; i < this.pose.length; ++i) {
-            if (distance(this.pose[i], click) < distance(this.pose[nearest], click)) {
+            if (distance(this.pose[i], pos) < distance(this.pose[nearest], pos)) {
                 nearest = i;
             }
         }
-        if (distance(this.pose[nearest], click) < 10*10) {
+        if (distance(this.pose[nearest], pos) < 10*10) {
             this.draggingVertex = nearest;
-            this.onDragVertex(click);
+            this.onDragVertex(pos);
         }
     }
 
@@ -95,12 +101,12 @@ class UI {
         if (this.draggingVertex === null) {
             return;
         }
-        const click = this.translator.canvasToModel([ev.offsetX, ev.offsetY]);
-        this.onDragVertex(click);
+        const pos = this.translator.canvasToModel([ev.offsetX, ev.offsetY]);
+        this.onDragVertex(pos);
     }
 
-    private onDragVertex(click: Point) {
-        console.log(this.draggingVertex, click);
+    private onDragVertex(pos: Point) {
+        this.pose[this.draggingVertex!] = roundPoint(pos);
         this.draw();
     }
 }
