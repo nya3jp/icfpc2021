ccdb7e3640ec01b9a8161b6f59f7f65c4ec26ac1
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2021-07-09 22:49:24 +0900

    add subcmd

--- third_party/tanakh-solver/src/main.rs
+++ third_party/tanakh-solver/src/main.rs
@@ -10,5 +10,23 @@ fn solve(problem_id: i64) -> Result<()> {
     Ok(())
 }
 
-#[argopt::cmd_group(commands = [solve])]
+#[argopt::subcmd(name = "max-scores")]
+fn max_scores() -> Result<()> {
+    println!("Max scores:");
+
+    for pid in 1..=59 {
+        let problem = get_problem(pid)?;
+        let max_score = 1000.0
+            * ((problem.figure.vertices.len() * problem.figure.edges.len() * problem.hole.len())
+                as f64
+                / 6.0)
+                .log2();
+
+        println!("Problem {}: {}", pid, max_score.ceil() as i64);
+    }
+
+    Ok(())
+}
+
+#[argopt::cmd_group(commands = [solve, max_scores])]
 fn main() -> Result<()> {}
