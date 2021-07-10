0677fc33dfb677ff48364d3abeb22ecebeb5b91c
phoenixstarhiro <phoenixstarhiro@users.noreply.github.com>
2021-07-10 14:00:59 +0900

    Add verbose

--- third_party/scorer/src/lib.rs
+++ third_party/scorer/src/lib.rs
@@ -20,29 +20,50 @@ pub fn dislike(hole: &Hole, pose: &Pose) -> usize {
     dislike as usize
 }
 
-pub fn is_inside_hole_partial(problem: &Problem, pose: &Pose, changed: &[usize]) -> bool {
+fn is_inside_hole_internal(
+    problem: &Problem,
+    pose: &Pose,
+    changed: Option<&[usize]>,
+    verbose: bool,
+) -> bool {
     // The vertices in pose should match with figure.
     if problem.figure.vertices.len() != pose.vertices.len() {
-        eprintln!(
-            "Edge mismatch: {:?}, {:?}",
-            problem.figure.vertices.len(),
-            pose.vertices.len()
-        );
+        if verbose {
+            eprintln!(
+                "Edge mismatch: {:?}, {:?}",
+                problem.figure.vertices.len(),
+                pose.vertices.len()
+            );
+        }
         return false;
     }
 
     // All points should be contained in the hole.
-    for i in changed.iter() {
-        let p = &pose.vertices[*i];
-        if problem.hole.polygon.contains(p) == ContainsResult::OUT {
-            // eprintln!("Point {:?} is not contained in hole:", Point::from(*p));
-            return false;
+    if changed.is_none() {
+        for i in 0..pose.vertices.len() {
+            let p = &pose.vertices[i];
+            if problem.hole.polygon.contains(p) == ContainsResult::OUT {
+                if verbose {
+                    eprintln!("Point {:?} is not contained in hole:", Point::from(*p));
+                }
+                return false;
+            }
+        }
+    } else {
+        for i in changed.unwrap() {
+            let p = &pose.vertices[*i];
+            if problem.hole.polygon.contains(p) == ContainsResult::OUT {
+                if verbose {
+                    eprintln!("Point {:?} is not contained in hole:", Point::from(*p));
+                }
+                return false;
+            }
         }
     }
 
     // All segments should not cross each other.
     for edge in &problem.figure.edges {
-        if !(changed.contains(&edge.v1) || changed.contains(&edge.v2)) {
+        if changed.is_none() || !(changed.unwrap().contains(&edge.v1) || changed.unwrap().contains(&edge.v2)) {
             continue;
         }
 
@@ -52,14 +73,18 @@ pub fn is_inside_hole_partial(problem: &Problem, pose: &Pose, changed: &[usize])
             let h1 = &problem.hole.polygon.vertices[i];
             let h2 = &problem.hole.polygon.vertices[(i + 1) % problem.hole.polygon.vertices.len()];
             if is_crossing(p1, p2, h1, h2) {
-                // eprintln!("Point {:?} {:?} is crossing with {:?} {:?}", p1, p2, h1, h2);
+                if verbose {
+                    eprintln!("Point {:?} {:?} is crossing with {:?} {:?}", p1, p2, h1, h2);
+                }
                 return false;
             }
         }
         // All mid points should be contained in the hole.
         let mid = (*p1 + *p2) / 2.;
         if problem.hole.polygon.contains(&mid) == ContainsResult::OUT {
-            // eprintln!("Mid point {:?} is not contained in the hole", mid);
+            if verbose {
+                eprintln!("Mid point {:?} is not contained in the hole", mid);
+            }
             return false;
         }
     }
@@ -67,48 +92,16 @@ pub fn is_inside_hole_partial(problem: &Problem, pose: &Pose, changed: &[usize])
     true
 }
 
+pub fn is_inside_hole_partial(problem: &Problem, pose: &Pose, changed: &[usize]) -> bool {
+    is_inside_hole_internal(problem, pose, Some(changed), false)
+}
+
 pub fn is_inside_hole(problem: &Problem, pose: &Pose) -> bool {
-    // The vertices in pose should match with figure.
-    if problem.figure.vertices.len() != pose.vertices.len() {
-        eprintln!(
-            "Edge mismatch: {:?}, {:?}",
-            problem.figure.vertices.len(),
-            pose.vertices.len()
-        );
-        return false;
-    }
+    is_inside_hole_internal(problem, pose, None, false)
+}
 
-    // All points should be contained in the hole.
-    for p in &pose.vertices {
-        if problem.hole.polygon.contains(&Point::from(*p)) == ContainsResult::OUT {
-            // eprintln!("Point {:?} is not contained in hole:", Point::from(*p));
-            return false;
-        }
-    }
-
-    // All segments should not cross each other.
-    for edge in &problem.figure.edges {
-        let p1 = Point::from(pose.vertices[edge.v1]);
-        let p2 = Point::from(pose.vertices[edge.v2]);
-        for i in 0..problem.hole.polygon.vertices.len() {
-            let h1 = Point::from(problem.hole.polygon.vertices[i]);
-            let h2 = Point::from(
-                problem.hole.polygon.vertices[(i + 1) % problem.hole.polygon.vertices.len()],
-            );
-            if is_crossing(&p1, &p2, &h1, &h2) {
-                // eprintln!("Point {:?} {:?} is crossing with {:?} {:?}", p1, p2, h1, h2);
-                return false;
-            }
-        }
-        // All mid points should be contained in the hole.
-        let mid = (p1 + p2) / 2.;
-        if problem.hole.polygon.contains(&mid) == ContainsResult::OUT {
-            // eprintln!("Mid point {:?} is not contained in the hole", mid);
-            return false;
-        }
-    }
-
-    true
+pub fn is_inside_hole_verbose(problem: &Problem, pose: &Pose) -> bool {
+    is_inside_hole_internal(problem, pose, None, true)
 }
 
 pub fn is_valid_solution(problem: &Problem, pose: &Pose) -> bool {
