bfc8eeed8f796ebe8f6f106174f260d7754824b6
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2021-07-10 15:29:07 +0900

    Improve upload_solution.py

--- third_party/dashboard/upload_solution.py
+++ third_party/dashboard/upload_solution.py
@@ -6,8 +6,8 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument('problem_id', metavar='N', type=int, help='problem ID')
 parser.add_argument('solution', metavar='FILE', type=str, help='solution JSON file')
-parser.add_argument('--tags', metavar='TAG', nargs='*', type=str, help='tag')
-parser.add_argument('--solution_set', default='', type=str, help='optional solution set')
+parser.add_argument('--tag', dest='tags', metavar='TAG', action='append', default=[], help='tag')
+parser.add_argument('--url', default='http://spweek.badalloc.com/api/solutions', help='API URL')
 
 args = parser.parse_args()
 subprocess.run([
@@ -17,8 +17,6 @@ subprocess.run([
     '-F',
     'tags=' + ','.join(args.tags),
     '-F',
-    'solution_set=' + args.solution_set,
-    '-F',
     'solution=@' + args.solution,
-    'http://spweek.badalloc.com/api/solutions',
+    args.url,
 ])
