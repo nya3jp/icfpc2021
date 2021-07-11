2095ec591cc50472587170ce531d86f807132b2b
Masaya Suzuki <masayasuzuki@google.com>
2021-07-10 18:18:56 -0700

    Use HashRouter
    In order to use the BrowserRouter, we need to change the server side to
    recognize certain child pages to serve the index page (imagine that
    somebody copies the URL and pass it to others).
    
    HashRouter doesn't have this problem. Just use it instead.

--- third_party/dashboard/front/src/App.tsx
+++ third_party/dashboard/front/src/App.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import {FrontPage} from './FrontPage';
 import {SolutionPage} from './SolutionPage';
 
