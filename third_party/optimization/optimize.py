3ba25d7ec50d6969b8a4c86b6b364413381551b9
bakaming <kaming.snishida@gmail.com>
2021-07-12 04:42:26 +0000

    fix some bugs in optimize.py

--- third_party/optimization/optimize.py
+++ third_party/optimization/optimize.py
@@ -4,15 +4,21 @@ import math
 
 #PROBLEM_SIZE = 1
 PROBLEM_SIZE = 132
+#PROBLEM_SIZE = 20
+
+# Base Data
 problems = dict()
 solutions = dict()
+
 globalBestDislike = dict()
 dependency = dict()
 states = dict()
 candidates = list()
 diffData = dict()
 decided = dict()
+constraint = dict()
 selected = list()
+seenCandidates = dict()
 kinds = ['GLOBALIST', 'BREAK_A_LEG', 'WALLHACK', 'SUPERFLEX']
 
 def getSolutionsJson(i):
@@ -46,13 +52,13 @@ def getProblemSize(problemId):
     return len(problems[problemId]['data']['hole']) * len(problems[problemId]['data']['figure']['vertices']) * len(problems[problemId]['data']['figure']['edges'])
 
 def getScore(problemId, dislike):
-    score = math.ceil(1000.0 * math.log2(getProblemSize(problemId)) * math.sqrt((globalBestDislike[problemId]+1.0)/(dislike+1.0)))
+    score = math.ceil(1000.0 * math.log2(getProblemSize(problemId)/6.0) * math.sqrt((globalBestDislike[problemId]+1.0)/(dislike+1.0)))
     return score
 
 def updateDict(orig, target):
     if 'score' not in orig:
         return target
-    if orig['socre'] > target('score'):
+    if orig['score'] > target['score']:
         return orig
     else:
         return target
@@ -68,49 +74,72 @@ def createDiffDataAndCandidate():
             candidate['catchedBonus'] = submit[3]
             candidate['usedBonus'] = submit[2]
             candidate['dislike'] = submit[1]
-            for usedBonus in submit[2]:
-                if usedBonus not in diffData[key]:
-                    diffData[key][usedBonus[0]] = dict()
-                for catchedBonus in submit[3]:
-                    diffData[key][usedBonus[0]][catchedBonus[0]] = candidate
-                if len(submit[3]) == 0:
-                    diffData[key][usedBonus[0]]['unused'] = candidate
+            candidate['minimal_dislike'] = globalBestDislike[key]
             if len(submit[2]) == 0:
                 if 'unused' not in diffData[key]:
-                    diffData[key]['unused'] = dict()
-                for catchedBonus in submit[3]:
-                    diffData[key]['unused'][catchedBonus[0]] = candidate
-                if len(submit[3]) == 0:
-                    diffData[key]['unused']['unused'] = candidate
+                    diffData[key]['unused'] = candidate
+                else:
+                    diffData[key]['unused'] = updateDict(diffData[key]['unused'], candidate)
+            if  'candidates' not in diffData[key]:
+                diffData[key]['candidates'] = [candidate]
+            else:
+                diffData[key]['candidates'].append(candidate)
+            seenCandidates[submit[0]] = False
             candidates.append(candidate)
 
-def calcup(candidate, bonus):
-    if 'unused' not in diffData[candidate['problemId']][bonus]:
-        return candidate['score']
-    else:
-        return candidate['score'] - diffData[candidate['problemId']][bonus]['unused']['score']
+def calcup(candidate):
+    return candidate['score'] - diffData[candidate['problemId']]['unused']['score']
     
-def calcdown(diff, bonus):
-    ret = 99999999
-    for i, sc in diff.items():
-        if i == 'unused':
-            continue
-        for j, catch in sc.items():
-            if j == bonus:
-                if 'unused' not in sc:
-                    return 0
-                else:
-                    ret = diff[bonus]['unused'] - catch
-    return ret
+def calcdown(prob, bonus):
+    ret = -999999999
+    # check diff that have valid constraint
+    if prob not in diffData:
+        return 999999999
+    for item in diffData[prob]['candidates']:
+        if seenCandidates[item['solutionId']]:
+                continue
+        valid = True
+        for b in item['catchedBonus']:
+            tmp = False
+            if b == bonus:
+                tmp = True
+            else:
+                for c in constraint[prob]:
+                    if b == c:
+                        tmp = True
+            valid = valid and tmp
+        if valid:
+            ret = max(ret, item['score'] - diffData[prob]['unused']['score'])
+    if ret == -999999999:
+        return 999999999
+    else:
+        return ret
 
 def decide(candidate):
     if candidate['problemId'] in decided:
         return
+
+    # reject candidates that have invalid catch Bonus
+    if candidate['problemId'] in constraint:
+        is_ret = True
+        for i in  constraint[candidate['problemId']]:
+            found = False
+            for j in candidate['catchedBonus']:
+                if j[0] == i[0]:
+                    found = True
+            is_ret = is_ret and found
+        if not is_ret:
+            seenCandidates[candidate['solutionId']] = True
+            return
+    else:
+        constraint[candidate['problemId']] = []
     if len(candidate['usedBonus']) == 0:
         print(candidate)
         selected.append(candidate)
         decided[candidate['problemId']] = candidate
+        seenCandidates[candidate['solutionId']] = True
         return
+
     bonus = candidate['usedBonus'][0][0]
     prob = candidate['usedBonus'][0][1]
     if prob in decided:
@@ -122,14 +151,16 @@ def decide(candidate):
             print(candidate)
             selected.append(candidate)
             decided[candidate['problemId']] = candidate
+            seenCandidates[candidate['solutionId']] = True
         return
     else:
-        if prob not in diffData:
-            return
-        if calcup(candidate, bonus) > calcdown(diffData[prob], bonus):
+        # TODO: see constraint dependency
+        if calcup(candidate) > calcdown(prob, bonus):
             print(candidate)
             selected.append(candidate)
             decided[candidate['problemId']] = candidate
+            if bonus not in constraint[candidate['problemId']]:
+                constraint[candidate['problemId']].append(bonus)
 
 def solve():
     sortedCandidates = sorted(candidates, key=lambda x:x['score'], reverse=True)
@@ -149,32 +180,48 @@ def getProblemJson(i):
 def getGlobalBestDislike(problem):
     globalBestDislike[problem['problem_id']] = problem['minimal_dislike']
 
+def updateGlobalBestDislike(problemNumber, solution):
+    for submit in solution:
+        dislike = submit['dislike']
+        globalBestDislike[problemNumber] = min(globalBestDislike[problemNumber], dislike)
+
 def createDependency(problem):
     if 'bonuses' in problem['data'] and problem['data']['bonuses'] is not None:
         for bonus in problem['data']['bonuses']:
             if bonus['problem'] not in dependency:
                 dependency[bonus['problem']] = []
-            dependency[bonus['problem']].append((problem, bonus['bonus']))
+            dependency[bonus['problem']].append((problem['problem_id'], bonus['bonus']))
+
+def getURL(sortedSelected):
+    url = 'https://spweek.badalloc.com/#/solutionsets/?id='
+    for i in sortedSelected:
+        url = url + str(i['solutionId']) + ','
+    return url[0:-1] 
+
 
 def main():
-    totalscore = 0
+    # Get Problems and Solutions
     for i in range(PROBLEM_SIZE):
         problemNumber = i+1
-        solutions[problemNumber] = getSolutionsJson(problemNumber)
         problems[problemNumber] = getProblemJson(problemNumber)
-        for key, problem in problems.items():
-            getGlobalBestDislike(problem)
-            createDependency(problem)
+        solutions[problemNumber] = getSolutionsJson(problemNumber)
+    for key, problem in problems.items():
+        getGlobalBestDislike(problem)
+        createDependency(problem)
 
     for problemNumber, solution in solutions.items():
         createStates(problemNumber, solution)
+        updateGlobalBestDislike(problemNumber, solution)
+
     createDiffDataAndCandidate()
     solve()
-    print(len(selected))
-    total = 0
+    sortedSelected = sorted(selected, key=lambda x:x['problemId'], reverse=True)
+    print(getURL(sortedSelected))
+    totalScore = 0
     for i in selected:
-        total = total + i['score']
-    print(total)
+        totalScore = totalScore + i['score']
+    print(totalScore)
+    #print(sortedSelected)
 
 if __name__ == "__main__":
     main()
